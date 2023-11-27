a104161fe90420f5e82fb752e417c9ee4f104e18
Adam De Fouw <aldefouw@medicine.wisc.edu>
2023-11-27 16:25:58 -0600

    Update user_rights.js

--- step_definitions/user_rights.js
+++ step_definitions/user_rights.js
@@ -156,7 +156,8 @@ Given("I save changes within the context of User Rights", () => {
 /**
  * @module UserRights
  * @author Adam De Fouw <aldefouw@medicine.wisc.edu>
- * @example I (add | remove) all basic user rights for the open User Rights dialog box
+ * @example I {userRightAction} all Basic Rights within the open User Rights dialog box
+ * @param {string} userRightAction - available options: 'add', 'remove'
  * @description Checks or Unchecks all Basic Rights within the User Rights dialog box.
  */
 Given('I {userRightAction} all Basic Rights within the open User Rights dialog box', (action) => {
@@ -192,8 +193,9 @@ Given('I {userRightAction} all Basic Rights within the open User Rights dialog b
 /**
  * @module UserRights
  * @author Corey DeBacker <debacker@wisc.edu>
- * @example I set Data Viewing Rights to < No Access | Read Only | View & Edit > for the instrument {string}
- * @param {dataViewingRights} level - the level of rights to be assigned
+ * @example I set Data Viewing Rights to {dataViewingRights}{editSurveyRights} for the instrument {string}
+ * @param {string} dataViewingRights - available options: 'No Access', 'Read Only', 'View & Edit', 'Edit survey responses'
+ * @param {string} editSurveyRights - available options: '', ' with Edit survey responses checked', ' with Edit survey responses unchecked', ''
  * @param {string} instrument - the label of the instrument for which to configure data entry rights
  * @description Selects a radio option for Data Entry Rights for the specified instrument within the user rights configuration dialog.
  */
