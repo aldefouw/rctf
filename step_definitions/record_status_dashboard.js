9a9313f877ea4437578e1286e1a3b61fd4947e30
Adam De Fouw <aldefouw@medicine.wisc.edu>
2023-11-27 16:26:01 -0600

    Update record_status_dashboard.js

--- step_definitions/record_status_dashboard.js
+++ step_definitions/record_status_dashboard.js
@@ -1,9 +1,9 @@
 /**
  * @module RecordStatusDashboard
  * @author Adam De Fouw <aldefouw@medicine.wisc.edu>
- * @example I click on the bubble for the {string} data collection instrument for record ID {string}
+ * @example I click on the bubble for the {string} data collection instrument for {recordIDEvent} {string}
  * @param {string} text - the text value of data collection instrument you want to target
- * @param {string} record_id - the value of the record_id you want to target
+ * @param {string} recordIDEvent - available options: 'record ID', 'event'
  * @description Clicks on a bubble within the Record Status Dashboard based upon record ID and the data instrument specified.
  */
 Given("I click on the bubble for the {string} data collection instrument for {recordIDEvent} {string}", (text, event_type, record_id) => {
@@ -15,11 +15,11 @@ Given("I click on the bubble for the {string} data collection instrument for {re
 /**
  * @module RecordStatusDashboard
  * @author Adam De Fouw <aldefouw@medicine.wisc.edu>
- * @example I locate the bubble for the {string} instrument on event {string} for record ID {string} {cellAction}
+ * @example I locate the bubble for the {string} instrument on event {string} for record ID {string}{cellAction}
  * @param {string} instrument - the data collection instrument you want to target
  * @param {string} event - the event name you want to target
  * @param {string} record_id - the value of the record_id you want to target
- * @param {string} cell_action - < and click the new instance link | and click on the bubble | and click the repeating instrument bubble for the (first | second | third) instance >
+ * @param {string} cellAction - available options: ' and click the new instance link', ' and click on the bubble', ' and click the repeating instrument bubble for the first instance', ' and click the repeating instrument bubble for the second instance', ' and click the repeating instrument bubble for the third instance'
  * @description Clicks on a bubble within the Record Status Dashboard based upon record ID and the longitudinal data instrument specified within an event.
  */
 
