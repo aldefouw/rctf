345137feef858d16d15f6ad0d926a277cc5673c3
Adam De Fouw <aldefouw@medicine.wisc.edu>
2023-12-19 14:24:27 -0600

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function intercept_vanderbilt_requests(){
     cy.intercept({ method: 'GET', url: '*/consortium/collect_stats.php?*'}, []).as('Stats')
     cy.intercept({ method: 'GET', url: '*/ControlCenter/check_server_ping.php'}, []).as('Ping')
     cy.intercept({ method: 'GET', url: '*/ControlCenter/report_site_stats.php'}, []).as('Control Center Stats')
-    cy.intercept({ method: 'GET', url: '**/*'}).as('interceptedRequest').then(() => {
+    cy.intercept({ method: 'GET', url: '*/redcap_v' + Cypress.env('redcap_version') + '/**'}).as('interceptedRequest').then(() => {
         window.registeredAlias = true // this is useful to know whether we can actually call a cy.wait
     })
 }
