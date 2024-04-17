9cf8edf05a9daa1efdc3bfe41c8c00d49819a1ca
Adam De Fouw <aldefouw@medicine.wisc.edu>
2024-04-17 09:45:55 -0500

    Update csv.js

--- step_definitions/csv.js
+++ step_definitions/csv.js
@@ -1,53 +1,71 @@
 /**
  * @module CSV
  * @author Tintin Nguyen <tin-tin.nguyen@nih.gov>
- * @example I (should) have a CSV file located at path {string} that contains {int} row(s)
- * @param {string} path - the text path of the CSV location
- * @param {DataTable} headings - the DataTable of headings this file should have
- * @description Verifies number of rows (excluding header) the CSV file should have.
+ * @example the downloaded CSV with filename {string}( should have)( has) {int} row(s)
+ * @param {string} filename - the filename the downloaded CSV
+ * @param {int} num_rows - the number of row sthe CSV file should have
+ * @description Verifies number of rows the CSV file should have.
  */
- Given("I (should )have a CSV file at path {string} that contains {int} row(s)", (path, headings) => {
-     cy.download_file(path).then( ($text) => {
-        let lines = $text.trim().split('\n')
-        let header_line = headings.rawTable[0][0]
-        for(let i = 1; i < headings.rawTable[0].length; i++){
-            header_line += "," + headings.rawTable[0][i]
-        }
-
-        expect(lines[0]).to.equal(header_line)
+ Given("the downloaded CSV with filename {string}( should have)( has) {int} row(s)", (filename, num_rows) => {
+     cy.download_file(filename).then( ($text) => {
+         cy.task('parseCsv', {csv_string: $text}).then((csv_rows) => {
+             expect(csv_rows.length).to.equal(num_rows)
+         })
     })
 })
 
 /**
  * @module CSV
  * @author Tintin Nguyen <tin-tin.nguyen@nih.gov>
- * @example the CSV file at path {string} has the headings below {DataTable}
- * @param {string} path - the text path of the CSV location
+ * @example the downloaded CSV with filename {string}( should have)( has) the( ){headerOrNot}( row)(s) below
+ * @param {string} filename - the filename the downloaded CSV
+ * @param {string} headerOrNot - available options: 'header and', 'header'
  * @param {DataTable} headings the DataTable of headings this file should have
- * @description Verifies headings of the CSV file.
+ * @description Verifies headers and rows of the CSV file.
  */
- Given("the CSV file at path {string} has the headings below", (path, headings) => {
-     cy.download_file(path).then( ($text) => {
-        let lines = $text.trim().split('\n')
-        let header_line = headings.rawTable[0][0]
-        for(let i = 1; i < headings.rawTable[0].length; i++){
-            header_line += "," + headings.rawTable[0][i]
-        }
+Given("the downloaded CSV with filename {string}( should have)( has) the( ){headerOrNot}( row)(s) below", (filename, header, table) => {
+    cy.download_file(filename).then( ($text) => {
+        cy.task('parseCsv', {csv_string: $text}).then((csv_rows) => {
+            const headings = csv_rows[0]
+            const table_rows = table.hashes()
+            let col_tables = {}
 
-        expect(lines[0]).to.equal(header_line)
+            if(header === "header and" || header === "header" ) {
+                for (let $i = 0; $i <= headings.length; $i++) {
+                    for (let i = 0; i < table.rawTable[0].length; i++) {
+                        if (table.rawTable[0][i] !== undefined && table.rawTable[0][i] === headings[$i]) {
+                            expect(headings[$i]).to.equal(table.rawTable[0][i])
+                            col_tables[table.rawTable[0][i]] = {index: $i}
+                        }
+                    }
+                }
+            }
+
+            if(header === "header and") {
+                for (let row = 0; row < table_rows.length; row++) {
+                    let csv_line = csv_rows[row + 1]
+                    let current_row = table_rows[row]
+
+                    for (let key = 0; key < table.rawTable[0].length; key++) {
+                        let col_key = table.rawTable[0][key]
+                        let col_index = col_tables[col_key]['index']
+                        expect(csv_line[col_index]).to.include(current_row[col_key])
+                    }
+                }
+            }
+        })
     })
 })
-
 /**
  * @module CSV
  * @author Tintin Nguyen <tin-tin.nguyen@nih.gov>
- * @example the CSV file at path {string} has a value {string} for column {string}
- * @param {string} path - the text path of the CSV location
+ * @example the downloaded CSV with filename {string}( should have)( has) a value {string} for column {string}
+ * @param {string} filename - the filename the downloaded CSV
  * @param {string} value - the value of the column data we are verifying
  * @param {string} column - the text name of the column data we are verifying
  * @description Verifies a row value exists for a given column within a CSV file.
  */
- Given("the CSV file at path {string} has a value {string} for column {string}", (path, value, column) => {
+ Given("the downloaded CSV with filename {string}( should have)( has) a value {string} for column {string}", (filename, value, column) => {
      function parseCSVRow(row) {
          // Regular expression to match commas not inside quotes
          const regex = /,\s*(?=(?:[^"]|"[^"]*")*$)/
@@ -58,70 +76,49 @@
          return cells
      }
 
-     cy.download_file(path).then(($text) => {
-         let lines = $text.trim().split('\n')
-         let columns = parseCSVRow(lines[0])
-         let index = columns.indexOf(column)
+     cy.download_file(filename).then(($text) => {
+         cy.task('parseCsv', {csv_string: $text}).then((csv_rows) => {
+             let lines = csv_rows
+             let columns = parseCSVRow(lines[0])
+             let index = columns.indexOf(column)
 
-         expect(index).to.be.greaterThan(-1)
+             expect(index).to.be.greaterThan(-1)
 
-         let found_value = { match: false, type: null, value: null }
-         let promise = Promise.resolve()
+             let found_value = { match: false, type: null, value: null }
+             let promise = Promise.resolve()
 
-         promise = promise.then(() => {
-             for (let i = 1; i < lines.length; i++) {
-                 let columns = parseCSVRow(lines[i])
-                 let columnValue = columns[index]
-                 if (columnValue.includes(value)) {
-                     found_value['match'] = true
-                     found_value['type'] = 'string'
-                     found_value['value'] = columnValue
-                     return Promise.resolve()
-                 } else if (window.dateFormats.hasOwnProperty(value)){
-                    const substring = columnValue.match(window.dateFormats[value])
-                    const isMatch = substring !== null
-                     if(isMatch){
+             promise = promise.then(() => {
+                 for (let i = 1; i < lines.length; i++) {
+                     let columns = parseCSVRow(lines[i])
+                     let columnValue = columns[index]
+                     if (columnValue.includes(value)) {
                          found_value['match'] = true
-                         found_value['type'] = 'datetime'
+                         found_value['type'] = 'string'
                          found_value['value'] = columnValue
                          return Promise.resolve()
+                     } else if (window.dateFormats.hasOwnProperty(value)){
+                         const substring = columnValue.match(window.dateFormats[value])
+                         const isMatch = substring !== null
+                         if(isMatch){
+                             found_value['match'] = true
+                             found_value['type'] = 'datetime'
+                             found_value['value'] = columnValue
+                             return Promise.resolve()
+                         }
                      }
                  }
-             }
-         })
+             })
 
-         promise.then(() => {
-             if (found_value['match'] && found_value['type'] === 'datetime') {
-                 expect(found_value['value']).to.match(window.dateFormats[value])
-             } else if (found_value['match']) {
-                 expect(found_value['value']).to.contain(value)
-             }
+             promise.then(() => {
+                 if (found_value['match'] && found_value['type'] === 'datetime') {
+                     expect(found_value['value']).to.match(window.dateFormats[value])
+                 } else if (found_value['match']) {
+                     expect(found_value['value']).to.contain(value)
+                 }
 
-         }).catch((error) => {
-             console.error("An error occurred:", error)
+             }).catch((error) => {
+                 console.error("An error occurred:", error)
+             })
          })
      })
-})
-
-/**
- * @module CSV
- * @author Tintin Nguyen <tin-tin.nguyen@nih.gov>
- * @example I remove line {int} from a CSV file at path {string}
- * @param {string} path - the text path of the CSV location
- * @param {string} value - the value of the column data we are verifying
- * @param {string} column - the text name of the column data we are verifying
- * @description Removes a line from the CSV file at the path specified.
- */
- Given("I remove line {int} from a CSV file at path {string}", (line, path) => {
-     cy.download_file(path).then( ($text) => {
-        let lines = $text.trim().split('\n')
-
-        let newLines = []
-        for(let i = 0; i < lines.length; i++){
-            if(i !== line-1){
-                newLines.push(lines[i])
-            }
-        }
-        cy.writeFile(path, newLines.join(',\n') + '\n')
-    })
 })
