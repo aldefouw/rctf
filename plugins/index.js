cb29559e919335e72464c81f21f0bebdba842556
Adam De Fouw <aldefouw@medicine.wisc.edu>
2024-05-06 15:28:46 -0500

    Update index.js

--- plugins/index.js
+++ plugins/index.js
@@ -7,21 +7,20 @@
 // You can read more here:
 // https://on.cypress.io/plugins-guide
 // ***********************************************************
-
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
-const cucumber = require('cypress-cucumber-preprocessor').default
 const shell = require('shelljs')
 const sed_lite = require('sed-lite').sed
 const fs = require('fs')
 const csv = require('async-csv')
 const path = require('path')
 
-module.exports = (on, config) => {
-    // `on` is used to hook into various events Cypress emits
-    // `config` is the resolved Cypress config
+const { addCucumberPreprocessorPlugin, defineParameterType, Given, When, Then } = require("@badeball/cypress-cucumber-preprocessor")
+const { preprocessor } = require("@badeball/cypress-cucumber-preprocessor/browserify")
 
-    on('file:preprocessor', cucumber())
+module.exports = (on, config) => {
+
+    addCucumberPreprocessorPlugin(on, config)
+
+    on("file:preprocessor", preprocessor(config))
 
     on('task', {
 
@@ -188,4 +187,6 @@ module.exports = (on, config) => {
         }
 
     })
+
+    return config
 }
