3e3754ba697d580f556805b03dee9352125bf9be
Adam De Fouw <aldefouw@medicine.wisc.edu>
2024-05-29 11:48:45 -0500

    Update visibilty.js
    Update all_mappings.js
    
    Update visibilty.js
    
    Update visibilty.js
    
    Update visibilty.js

--- step_definitions/support/all_mappings.js
+++ step_definitions/support/all_mappings.js
@@ -71,7 +71,7 @@ window.parameterTypes = {
     labeledExactly: ['labeled', 'labeled exactly', 'in the row labeled', 'for the instrument row labeled', 'for the variable', 'for the File Repository file named', 'for Data Quality Rule #', "within the Record Locking Customization table for the Data Collection Instrument named", "in the data entry form field", 'for the Event named', 'longitudinal instrument on event'],
     linkNames: ['link', 'tab', 'instrument', 'icon'],
     projectRequestLabel: ['Create Project', 'Send Request'],
-    notSee: ['not '],
+    notSee: ['not'],
     ordering: ['ascending', 'descending'],
     onlineDesignerButtons: ['"Enable"', '"Disable"', '"Choose action"', '"Survey settings"', '"Automated Invitations"', 'enabled survey icon', '"View Report"', '"Execute"', '"Save"'],
     projectStatus: ['Production', 'Development', 'Analysis/Cleanup'],
