a019d6bca56e1b92f662a378b77c9a5f4ba991e8
Adam De Fouw <aldefouw@medicine.wisc.edu>
2024-05-31 12:14:13 -0500

    Update visibilty.js

--- step_definitions/visibilty.js
+++ step_definitions/visibilty.js
@@ -224,6 +224,11 @@ Given("I (should ){notSee}see( ){articleType}( ){visibilityPrefix}( ){onlineDesi
 
     if(el !== ''){ subsel = {'link': 'a', 'button': 'button', 'field': 'tr', 'section break': 'td.header'}[el] }
 
+    //It's possible for the survey icon to appear even if the instrument itself is not a link
+    if(el === 'link' && online_buttons.includes('survey icon')){
+        subsel = ''
+    }
+
     let disabled_status = disabled_text !== undefined && disabled_text === "is disabled" ? ':disabled': ':not([disabled])'
 
     // double quotes need to be re-escaped before inserting into :contains() selector
