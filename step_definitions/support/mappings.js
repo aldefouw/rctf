1cb7a8c48c39fdf64d6a5efd44b00d9a3825d69d
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2024-10-29 17:24:12 -0700

    fix: adds mapping for Email Logging in User Rights

--- step_definitions/support/mappings.js
+++ step_definitions/support/mappings.js
@@ -23,7 +23,8 @@ window.userRightChecks = {
     'API Import/Update' : 'api_import',
     'REDCap Mobile App - Allow users to collect data offline in the mobile app' : 'mobile_app',
     'REDCap Mobile App - Allow user to download data for all records to the app?' : 'mobile_app_download_data',
-    'Calendar & Scheduling' : 'calendar'
+    'Calendar & Scheduling' : 'calendar',
+    'Email Logging' : 'email_logging'
 }
 
 window.singleChoiceMappings = {
