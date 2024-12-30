6351a65b6a5c65dc2022095ed156029bfd24a1a1
Mark McEver <mark@fastmail.net>
2024-12-29 22:13:35 -0600

    Made it possible to run crons

--- step_definitions/visit_page.js
+++ step_definitions/visit_page.js
@@ -10,3 +10,17 @@ Given("I visit the REDCap login page", () => {
     cy.logout()
     cy.visit_version({page: '/'})
 })
+
+/**
+ * @module VisitPage
+ * @author Mark McEver <mark.mcever@vumc.org>
+ * @example I wait for background processes to finish
+ * @description Executes REDCap's crons and returns to the previous page
+ */
+Given("I wait for background processes to finish", () => {
+    cy
+        .visit('/cron.php')
+        .get('body')
+        .contains('Completed all jobs!')
+        .go('back')
+})
